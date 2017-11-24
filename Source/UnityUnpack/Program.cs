54462f3558d92b3d5ff3bbdf248c36bc08fec8f7
Fatih BAKIR <bakir@ucsb.edu>
2017-11-23 19:27:08 -0800

    library, pack, unpack

---
+++ Source/UnityUnpack/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UnityPacker
+{
+    internal class UnpackProgram
+    {
+        public static void Main(string[] args)
+        {   
+            var p = Package.FromPackage(args[0]);
+            p.GenerateFolder(args.Length > 0 ? args[1] : "");
+        }
+    }
+}
